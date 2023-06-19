b0640ed18096ffa7d16147ced34fc45d26d15219
arhor <arhor18@gmail.com>
2023-06-19 13:36:32 +0200

    apollo-server wip security;

--- apollo-server/index.js
+++ apollo-server/index.js
@@ -1,32 +1,35 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 
-const { url } = await startStandaloneServer(
-    new ApolloServer({
-        gateway: new ApolloGateway({
-            supergraphSdl: new IntrospectAndCompose({
-                subgraphs: [
-                    {
-                        name: 'users',
-                        url: 'http://localhost:5001/graphql',
-                    },
-                    {
-                        name: 'articles',
-                        url: 'http://localhost:5002/graphql',
-                    },
-                    {
-                        name: 'comments',
-                        url: 'http://localhost:5003/graphql',
-                    },
-                    {
-                        name: 'extraData',
-                        url: 'http://localhost:5004/graphql',
-                    },
-                ],
-            }),
+const server = new ApolloServer({
+    gateway: new ApolloGateway({
+        supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+                { url: 'http://localhost:5001/graphql', name: 'users' },
+                { url: 'http://localhost:5002/graphql', name: 'articles' },
+                { url: 'http://localhost:5003/graphql', name: 'comments' },
+                { url: 'http://localhost:5004/graphql', name: 'extradata' },
+            ],
         }),
-    })
-);
+        buildService: ({ url }) => new RemoteGraphQLDataSource({
+            url,
+            willSendRequest: ({ request, context }) => {
+                if (context.user) {
+                    request.http.headers.set('user', JSON.stringify(context.user));
+                }
+            }
+        })
+    }),
+});
 
-console.log(`🚀  Server ready at ${url}`);
+const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => {
+        const token = req.headers.authorization || '';
+        const user = await getUser(token);
+
+        return { user };
+    },
+});
+
+console.log(`🚀 Server listening at: ${url}`);
