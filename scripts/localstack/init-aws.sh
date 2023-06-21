2a653b4c0bbce940bdf08365062b8396088d81c8
arhor <arhor18@gmail.com>
2023-06-21 16:29:28 +0200

    WIP on infra;

--- scripts/localstack/init-aws.sh
+++ scripts/localstack/init-aws.sh
@@ -2,6 +2,23 @@
 
 ### User ###
 
+awslocal sns create-topic --name user-updated-events --output table | cat
+
+awslocal sqs create-queue --queue-name user-updated-events-1 --output table | cat
+awslocal sqs create-queue --queue-name user-updated-events-2 --output table | cat
+
+awslocal sns subscribe \
+    --topic-arn "arn:aws:sns:us-east-1:000000000000:user-updated-events" \
+    --protocol sqs \
+    --notification-endpoint "arn:aws:sqs:us-east-1:000000000000:user-updated-events-1" \
+    --output table | cat
+
+awslocal sns subscribe \
+    --topic-arn "arn:aws:sns:us-east-1:000000000000:user-updated-events" \
+    --protocol sqs \
+    --notification-endpoint "arn:aws:sqs:us-east-1:000000000000:user-updated-events-1" \
+    --output table | cat
+
 awslocal sns create-topic --name user-deleted-events --output table | cat
 
 awslocal sqs create-queue --queue-name user-deleted-events-1 --output table | cat
