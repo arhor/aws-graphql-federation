c21b7b977b16b2605d87268302118ed7c31cfe48
arhor <arhor18@gmail.com>
2023-07-03 12:18:23 +0200

    SQS tests WIP;

--- dgs-comments/src/test/kotlin/com/github/arhor/dgs/comments/api/listener/UserChangeSqsListenerTest.kt
+++ dgs-comments/src/test/kotlin/com/github/arhor/dgs/comments/api/listener/UserChangeSqsListenerTest.kt
@@ -1,32 +1,38 @@
 package com.github.arhor.dgs.comments.api.listener
 
-import com.github.arhor.dgs.comments.api.listener.BaseSqsListenerTest.Companion.POST_DELETED_TEST_EVENTS_QUEUE
-import com.github.arhor.dgs.comments.api.listener.BaseSqsListenerTest.Companion.POST_UPDATED_TEST_EVENTS_QUEUE
-import com.github.arhor.dgs.comments.api.listener.BaseSqsListenerTest.Companion.USER_DELETED_TEST_EVENTS_QUEUE
-import com.github.arhor.dgs.comments.api.listener.BaseSqsListenerTest.Companion.USER_UPDATED_TEST_EVENTS_QUEUE
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import com.github.arhor.dgs.comments.service.CommentService
 import com.ninjasquad.springmockk.MockkBean
 import io.awspring.cloud.sqs.operations.SqsOperations
 import io.awspring.cloud.test.sqs.SqsTest
 import io.mockk.every
+import io.mockk.just
+import io.mockk.runs
 import io.mockk.verify
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.localstack.LocalStackContainer
+import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
 @DirtiesContext
 @Testcontainers(disabledWithoutDocker = true)
-@ContextConfiguration(classes = [BaseSqsListenerTest.Config::class])
-@SqsTest(listeners = [UserChangeSqsListener::class])
+@ContextConfiguration(classes = [UserChangeSqsListenerTest.Config::class])
+@SqsTest(listeners = [PostChangeSqsListener::class, UserChangeSqsListener::class])
 internal class UserChangeSqsListenerTest {
+
+    class Config {
+        @Bean
+        fun kotlinModuleBean() = kotlinModule()
+    }
 
     @Autowired
     private lateinit var sqs: SqsOperations
@@ -45,7 +51,7 @@ internal class UserChangeSqsListenerTest {
         val userId = 1L
         val event = UserChangeSqsListener.UserChange.Deleted(id = userId)
 
-        every { mockCommentService.unlinkCommentsFromUser(any()) }
+        every { mockCommentService.unlinkCommentsFromUser(any()) } just runs
 
         // When
         println(sqs.send(USER_DELETED_TEST_EVENTS_QUEUE, event))
@@ -57,12 +63,43 @@ internal class UserChangeSqsListenerTest {
         println("----------------------------------------------------------------")
     }
 
+    @Test
+    fun `should call deleteCommentsFromPost method on the CommentService on Post Deleted event`() {
+        // Given
+        println("----------------------------------------------------------------")
+        println("----------------------------------------------------------------")
+        println("----------------------------------------------------------------")
+        println(sqs)
+
+        val postId = 1L
+        val event = PostChangeSqsListener.PostChange.Deleted(id = postId)
+
+        every { mockCommentService.deleteCommentsFromPost(any()) } just runs
+
+        // When
+        sqs.send(POST_DELETED_TEST_EVENTS_QUEUE, event)
+
+        // Then
+        verify(exactly = 1, timeout = 10_000) { mockCommentService.deleteCommentsFromPost(postId) }
+
+        println("----------------------------------------------------------------")
+        println("----------------------------------------------------------------")
+        println("----------------------------------------------------------------")
+    }
+
     companion object {
+        private const val USER_UPDATED_TEST_EVENTS_QUEUE = "user-updated-test-events"
+        private const val USER_DELETED_TEST_EVENTS_QUEUE = "user-deleted-test-events"
+        private const val POST_UPDATED_TEST_EVENTS_QUEUE = "post-updated-test-events"
+        private const val POST_DELETED_TEST_EVENTS_QUEUE = "post-deleted-test-events"
+
         @JvmStatic
         @Container
         private val localstack =
             LocalStackContainer(DockerImageName.parse("localstack/localstack:1.4.0"))
+                .withClasspathResourceMapping("/localstack", "/docker-entrypoint-initaws.d", BindMode.READ_ONLY)
                 .withServices(LocalStackContainer.Service.SQS)
+                .waitingFor(Wait.forLogMessage(".*Initialized\\.\n", 1))
 
         @JvmStatic
         @DynamicPropertySource
@@ -76,17 +113,6 @@ internal class UserChangeSqsListenerTest {
                 add("app-props.aws.sqs.user-deletes") { USER_DELETED_TEST_EVENTS_QUEUE }
                 add("app-props.aws.sqs.post-updates") { POST_UPDATED_TEST_EVENTS_QUEUE }
                 add("app-props.aws.sqs.post-deletes") { POST_DELETED_TEST_EVENTS_QUEUE }
-            }
-        }
-
-        @JvmStatic
-        @BeforeAll
-        fun createTestQueues() {
-            with(localstack) {
-                execInContainer("awslocal", "sqs", "create-queue", "--queue-name", USER_UPDATED_TEST_EVENTS_QUEUE)
-                execInContainer("awslocal", "sqs", "create-queue", "--queue-name", USER_DELETED_TEST_EVENTS_QUEUE)
-                execInContainer("awslocal", "sqs", "create-queue", "--queue-name", POST_UPDATED_TEST_EVENTS_QUEUE)
-                execInContainer("awslocal", "sqs", "create-queue", "--queue-name", POST_DELETED_TEST_EVENTS_QUEUE)
             }
         }
     }
