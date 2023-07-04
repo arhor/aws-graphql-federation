35456f4777fdcf9300f3f9ff5d296400a84193bd
arhor <arhor18@gmail.com>
2023-07-04 17:14:26 +0200

    fixing bugs;

--- scripts/localstack/init-aws.sh
+++ scripts/localstack/init-aws.sh
@@ -7,11 +7,11 @@ user_events_arn=$(awslocal sns create-topic --name user-events --output text)
 user_created_events_for_posts_url=$(awslocal sqs create-queue --queue-name user-created-events-for-posts --output text)
 user_deleted_events_for_posts_url=$(awslocal sqs create-queue --queue-name user-deleted-events-for-posts --output text)
 
-user_created_events_for_comments_url=$(awslocal sqs create-queue --queue-name user-created-events-for-comments --output text)
-user_deleted_events_for_comments_url=$(awslocal sqs create-queue --queue-name user-deletes-events-for-comments --output text)
-
 user_created_events_for_posts_arn=$(awslocal sqs get-queue-attributes --queue-url "$user_created_events_for_posts_url" --attribute-names QueueArn --query Attributes --output text)
 user_deleted_events_for_posts_arn=$(awslocal sqs get-queue-attributes --queue-url "$user_deleted_events_for_posts_url" --attribute-names QueueArn --query Attributes --output text)
+
+user_created_events_for_comments_url=$(awslocal sqs create-queue --queue-name user-created-events-for-comments --output text)
+user_deleted_events_for_comments_url=$(awslocal sqs create-queue --queue-name user-deleted-events-for-comments --output text)
 
 user_created_events_for_comments_arn=$(awslocal sqs get-queue-attributes --queue-url "$user_created_events_for_comments_url" --attribute-names QueueArn --query Attributes --output text)
 user_deleted_events_for_comments_arn=$(awslocal sqs get-queue-attributes --queue-url "$user_deleted_events_for_comments_url" --attribute-names QueueArn --query Attributes --output text)
