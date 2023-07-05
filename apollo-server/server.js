7f0f778800d9d524ff329aeade39781aae04f5c5
arhor <arhor18@gmail.com>
2023-07-05 16:38:34 +0200

    js server refactoring;

--- apollo-server/server.js
+++ apollo-server/server.js
@@ -3,16 +3,31 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { v4 as uuid } from 'uuid';
 
+function error(message) {
+    throw Error(message);
+}
+
+function requiredEnv(variable) {
+    return process.env[variable] || error(`Missing env variable: ${variable}`);
+}
+
 const gateway = new ApolloGateway({
+    /**
+     * This property defines Supergraph Schema, composing it from subgraphs in downstream services. 
+     */
     supergraphSdl: new IntrospectAndCompose({
         subgraphs: [
-            { url: 'http://localhost:5001/graphql', name: 'users' },
-            { url: 'http://localhost:5002/graphql', name: 'posts' },
-            { url: 'http://localhost:5003/graphql', name: 'comments' },
+            { url: requiredEnv('SUBGRAPH_URL_USERS'), name: 'users' },
+            { url: requiredEnv('SUBGRAPH_URL_POSTS'), name: 'posts' },
+            { url: requiredEnv('SUBGRAPH_URL_COMMS'), name: 'comments' },
         ],
     }),
     buildService: ({ url }) => new RemoteGraphQLDataSource({
         url,
+
+        /**
+         * This function will be invoked before every request to downstream services.
+         */
         willSendRequest: ({ request, context }) => {
             request.http.headers.set('x-request-id', context.globalRequestId ?? uuid());
         },
@@ -24,6 +39,10 @@ const server = new ApolloServer({
 });
 
 const { url } = await startStandaloneServer(server, {
+    /**
+     * This function allows to define execution context of incoming request.
+     * It will be invoked once before processing.
+     */
     context: () => {
         return {
             globalRequestId: uuid(),
