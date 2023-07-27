ae228dbb9499376bbb6cee39771fbf1e8e2a2525
arhor <arhor18@gmail.com>
2023-07-27 10:08:05 +0200

    adjusted JWT service test;

--- dgs-users/src/test/kotlin/com/github/arhor/dgs/users/service/TokenProviderTest.kt
+++ dgs-users/src/test/kotlin/com/github/arhor/dgs/users/service/TokenProviderTest.kt
@@ -1,8 +1,10 @@
 package com.github.arhor.dgs.users.service
 
 import com.github.arhor.dgs.users.service.security.convertToRsaPublicKey
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.longs.shouldBeExactly
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.ComponentScan.Filter
@@ -10,38 +12,19 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import kotlin.time.Duration.Companion.minutes
 
-@SpringJUnitConfig
-internal class TokenProviderTest {
+internal class TokenProviderTest(
+    @Autowired
+    private val tokenProvider: TokenProvider
+) : DescribeSpec() {
 
     @Configuration
     @ComponentScan(
-        includeFilters = [
-            Filter(type = ASSIGNABLE_TYPE, classes = [TokenProvider::class])
-        ],
+        includeFilters = [Filter(type = ASSIGNABLE_TYPE, classes = [TokenProvider::class])],
         useDefaultFilters = false,
     )
     class Config
-
-    @Autowired
-    private lateinit var tokenProvider: TokenProvider
-
-    @Test
-    fun `should pass`() {
-        // Given
-        val rsaPubKey = tokenProvider.activePublicKey().convertToRsaPublicKey()
-        val jwtParser = Jwts.parserBuilder().setSigningKey(rsaPubKey).build()
-
-        // When
-        val signedJwt = tokenProvider.createSignedJwt()
-        val parsedJwt = jwtParser.parse(signedJwt)
-
-        // Then
-        println(signedJwt)
-        println(parsedJwt)
-    }
 
     companion object {
         private val JWT_VALIDITY_DURATION = 10.minutes
@@ -50,7 +33,24 @@ internal class TokenProviderTest {
         @DynamicPropertySource
         fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
             with(registry) {
-                add("app-props.jwt.expire") { JWT_VALIDITY_DURATION.toIsoString() }
+                add("app-props.jwt.expire", JWT_VALIDITY_DURATION::toIsoString)
+            }
+        }
+    }
+
+    init {
+        it("should create jwt with expected validity duration") {
+            // Given
+            val rsaPubKey = tokenProvider.activePublicKey().convertToRsaPublicKey()
+            val jwtParser = Jwts.parserBuilder().setSigningKey(rsaPubKey).build()
+
+            // When
+            val signedJwt = tokenProvider.createSignedJwt()
+            val parsedJwt = jwtParser.parse(signedJwt)
+
+            // Then
+            with(parsedJwt.body as Claims) {
+                (expiration.time - issuedAt.time) shouldBeExactly JWT_VALIDITY_DURATION.inWholeMilliseconds
             }
         }
     }
