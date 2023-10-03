ca7c89edfbce845b37e2745b213b5e2a03a73352
arhor <arhor18@gmail.com>
2023-10-03 23:16:05 +0200

    project restructured+1;

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+includeBuild("lib-platform")
+includeBuild("lib-common")
+includeBuild("lib-security")
+
+includeBuild("app-service-users")
+includeBuild("app-service-posts")
+includeBuild("app-service-comments")
