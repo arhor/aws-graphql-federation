151ed25ce637be86ce2a7f8a28d7b01234fc1735
arhor <arhor18@gmail.com>
2023-11-02 18:15:46 +0100

    added router test;

--- app-service-users/src/test/kotlin/com/github/arhor/aws/graphql/federation/users/api/router/MainRouterTest.kt
+++ app-service-users/src/test/kotlin/com/github/arhor/aws/graphql/federation/users/api/router/MainRouterTest.kt
@@ -1,14 +1,16 @@
 package com.github.arhor.aws.graphql.federation.users.api.router
 
+import com.github.arhor.aws.graphql.federation.security.CurrentUser
 import com.github.arhor.aws.graphql.federation.users.api.routes.MainRouter
 import com.github.arhor.aws.graphql.federation.users.service.UserService
+import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.from
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
-import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.web.servlet.function.ServerRequest
 import java.net.URI
@@ -16,9 +18,11 @@ import java.net.URI
 internal class MainRouterTest {
 
     private val userService = mockk<UserService>()
-
     private val mainRouter = MainRouter(
         userService,
+    )
+    private val messageConverters = listOf(
+        MappingJackson2HttpMessageConverter(),
     )
 
     @Test
@@ -28,7 +32,7 @@ internal class MainRouterTest {
         val httpMethod = HttpMethod.GET
         val httpStatus = HttpStatus.NO_CONTENT
 
-        val request = createMockServerRequest(httpMethod, requestURI)
+        val request = createMockServerRequest(requestURI, httpMethod)
 
         // When
         val response = mainRouter.route(request).map { it.handle(request) }
@@ -41,10 +45,42 @@ internal class MainRouterTest {
             .returns(httpStatus, from { it.statusCode() })
     }
 
-    private fun createMockServerRequest(httpMethod: HttpMethod, requestURI: URI): ServerRequest {
-        val servletRequest = MockHttpServletRequest(httpMethod.name(), requestURI.toString())
-        val messageReaders = emptyList<HttpMessageConverter<*>>()
+    @Test
+    fun `should handle user verification requesto`() {
+        // Given
+        val requestURI = URI.create("/api/users/verify")
+        val httpMethod = HttpMethod.POST
+        val httpStatus = HttpStatus.OK
 
-        return ServerRequest.create(servletRequest, messageReaders)
+        val request = createMockServerRequest(
+            requestURI,
+            httpMethod,
+            """
+            {
+                "username": "test-username",
+                "password": "test-password"
+            }
+            """.trimIndent()
+        )
+
+        every { userService.verifyUser(any()) } returns CurrentUser(id = -1L, authorities = listOf("ROLE_TEST"))
+
+        // When
+        val response = mainRouter.route(request).map { it.handle(request) }
+
+        // Then
+        assertThat(response)
+            .isNotNull
+            .isNotEmpty
+            .get()
+            .returns(httpStatus, from { it.statusCode() })
     }
+
+    private fun createMockServerRequest(uri: URI, method: HttpMethod, content: String? = null): ServerRequest =
+        ServerRequest.create(
+            MockHttpServletRequest(method.name(), uri.toString())
+                .also { it.setContent(content?.encodeToByteArray()) }
+                .also { it.contentType = "application/json" },
+            messageConverters,
+        )
 }
