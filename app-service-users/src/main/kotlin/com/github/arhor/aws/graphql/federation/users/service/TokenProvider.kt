b5d7d4f56c04cbbfb926c94e5cb9e6c6154839dd
arhor <arhor18@gmail.com>
2023-11-12 10:48:28 +0100

    * removed jjwt dependency;

--- app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/service/TokenProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.github.arhor.aws.graphql.federation.users.service
-
-import io.jsonwebtoken.JwtBuilder
-
-interface TokenProvider {
-
-    fun createSignedJwt(customize: JwtBuilder.() -> Unit = {}): String
-    fun activePublicKey(): String
-}
