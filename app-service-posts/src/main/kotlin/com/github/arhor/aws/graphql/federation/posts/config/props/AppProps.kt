510ff5d216fbb57591bd7fc7c1c7886aca4557e1
arhor <arhor18@gmail.com>
2023-12-27 16:43:09 +0100

    restructuring / refactoring =)

--- app-service-posts/src/main/kotlin/com/github/arhor/aws/graphql/federation/posts/config/props/AppProps.kt
+++ app-service-posts/src/main/kotlin/com/github/arhor/aws/graphql/federation/posts/config/props/AppProps.kt
@@ -2,16 +2,22 @@ package com.github.arhor.aws.graphql.federation.posts.config.props
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
+@Suppress("unused")
 @ConfigurationProperties(prefix = "app-props")
 data class AppProps(
     val aws: Aws,
 ) {
     data class Aws(
         val sns: Sns,
+        val sqs: Sns,
         val s3: S3,
     ) {
         data class Sns(
             val postEvents: String,
+        )
+
+        data class Sqs(
+            val userDeletedEvents: String,
         )
 
         data class S3(
