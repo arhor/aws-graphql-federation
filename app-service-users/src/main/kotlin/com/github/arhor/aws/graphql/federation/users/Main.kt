35afa12f12ea7bbe83eefd2c1b7f1c0bb543c7ca
arhor <arhor18@gmail.com>
2023-12-27 13:28:24 +0100

    restructuring / refactoring =)

--- app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/Main.kt
+++ app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/Main.kt
@@ -1,37 +1,15 @@
 package com.github.arhor.aws.graphql.federation.users
 
 import com.github.arhor.aws.graphql.federation.users.config.props.AppProps
-import org.slf4j.LoggerFactory
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.context.WebServerApplicationContext
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
 import org.springframework.retry.annotation.EnableRetry
-import org.springframework.web.context.WebApplicationContext
-import java.lang.invoke.MethodHandles
-
-private val logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
 
 @EnableRetry
 @EnableConfigurationProperties(AppProps::class)
 @SpringBootApplication
-class Main {
-
-    @Bean
-    @Profile("dev")
-    fun <T> displayApplicationInfo(context: T)
-        where T : WebApplicationContext,
-              T : WebServerApplicationContext = ApplicationRunner {
-
-        val port = context.webServer.port
-        val path = context.servletContext?.contextPath ?: ""
-
-        logger.info("Local access URL: http://localhost:{}{}", port, path)
-    }
-}
+class Main
 
 fun main(args: Array<String>) {
     runApplication<Main>(*args)
--- lib-spring-webmvc-config/src/main/kotlin/com/github/arhor/aws/graphql/federation/async/ConfigureAdditionalBeans.kt
+++ lib-spring-webmvc-config/src/main/kotlin/com/github/arhor/aws/graphql/federation/async/ConfigureAdditionalBeans.kt
@@ -1,7 +1,12 @@
 package com.github.arhor.aws.graphql.federation.async
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.web.context.WebServerApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.context.WebApplicationContext
 import java.time.Clock
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
@@ -10,11 +15,25 @@ import java.util.function.Supplier
 @Configuration(proxyBeanMethods = false)
 class ConfigureAdditionalBeans {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @Bean
     fun currentDateTimeSupplier() = Supplier {
         val systemUTC = Clock.systemUTC()
         val timestamp = LocalDateTime.now(systemUTC)
 
         timestamp.truncatedTo(ChronoUnit.MILLIS)
+    }
+
+    @Bean
+    @Profile("dev")
+    fun <T> displayApplicationInfo(context: T)
+        where T : WebApplicationContext,
+              T : WebServerApplicationContext = ApplicationRunner {
+
+        val port = context.webServer.port
+        val path = context.servletContext?.contextPath ?: ""
+
+        logger.info("Local access URL: http://localhost:{}{}", port, path)
     }
 }
