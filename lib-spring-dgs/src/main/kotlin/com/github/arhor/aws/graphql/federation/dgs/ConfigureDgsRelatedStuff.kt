dd911158ea0af5c07101bfeb74d72ff190342067
arhor <arhor18@gmail.com>
2024-01-18 14:02:30 +0100

    restructuring / refactoring =)

---
+++ lib-spring-dgs/src/main/kotlin/com/github/arhor/aws/graphql/federation/dgs/ConfigureDgsRelatedStuff.kt
@@ -0,0 +1,9 @@
+package com.github.arhor.aws.graphql.federation.dgs
+
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+
+@ComponentScan
+@AutoConfiguration
+class ConfigureDgsRelatedStuff {
+}
