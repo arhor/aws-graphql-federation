74a0a66bbddd0c6ea6bb72991dab73efb531e7c7
arhor <arhor18@gmail.com>
2024-01-23 15:49:33 +0100

    refactored listeners;

--- app-service-comments/src/main/java/com/github/arhor/aws/graphql/federation/comments/api/listener/UserEventsListener.java
+++ app-service-comments/src/main/java/com/github/arhor/aws/graphql/federation/comments/api/listener/UserEventsListener.java
@@ -3,6 +3,7 @@ package com.github.arhor.aws.graphql.federation.comments.api.listener;
 import com.github.arhor.aws.graphql.federation.comments.service.CommentService;
 import com.github.arhor.aws.graphql.federation.common.event.UserEvent;
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -10,14 +11,11 @@ import org.springframework.stereotype.Component;
 import java.lang.invoke.MethodHandles;
 
 @Component
+@RequiredArgsConstructor
 public class UserEventsListener {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final CommentService commentService;
-
-    public UserEventsListener(final CommentService commentService) {
-        this.commentService = commentService;
-    }
 
     @SqsListener("${app-props.aws.sqs.user-deleted-events}")
     public void handleUserDeletedEvent(final UserEvent.Deleted event) {
