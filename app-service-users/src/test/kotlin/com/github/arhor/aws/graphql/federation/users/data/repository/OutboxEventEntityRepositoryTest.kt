51e1ff127d4a8c38bb57cc2c79773d70508eeb6c
arhor <arhor18@gmail.com>
2024-01-25 13:47:18 +0100

    outbox events WIP;

--- app-service-users/src/test/kotlin/com/github/arhor/aws/graphql/federation/users/data/repository/OutboxEventEntityRepositoryTest.kt
+++ app-service-users/src/test/kotlin/com/github/arhor/aws/graphql/federation/users/data/repository/OutboxEventEntityRepositoryTest.kt
@@ -1,5 +1,7 @@
 package com.github.arhor.aws.graphql.federation.users.data.repository
 
+import com.github.arhor.aws.graphql.federation.users.data.entity.OutboxEventEntity
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -11,10 +13,34 @@ internal class OutboxEventEntityRepositoryTest(
     @Test
     fun `should return true for the email of an existing user`() {
         // given
+        outboxEventRepository.saveAll(
+            sequence {
+                repeat(10) {
+                    yield(
+                        OutboxEventEntity(
+                            type = "test-event",
+                            payload = emptyMap(),
+                            headers = emptyMap(),
+                        )
+                    )
+                }
+            }.toList()
+        )
 
         // when
+        val outboxEvents1 = outboxEventRepository.dequeueOldest(5)
+        val outboxEvents2 = outboxEventRepository.dequeueOldest(5)
 
         // then
+        assertThat(outboxEvents1)
+            .isNotNull()
+            .hasSize(5)
 
+        assertThat(outboxEvents2)
+            .isNotNull()
+            .hasSize(5)
+
+        assertThat(outboxEvents1)
+            .doesNotContainAnyElementsOf(outboxEvents2)
     }
 }
