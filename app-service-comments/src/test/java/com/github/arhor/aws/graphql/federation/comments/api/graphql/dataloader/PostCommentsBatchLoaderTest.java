385b35e9b8cfe3cd58282846a8500e2e0b0b1721
arhor <arhor18@gmail.com>
2024-01-31 15:34:04 +0100

    WIP;

--- app-service-comments/src/test/java/com/github/arhor/aws/graphql/federation/comments/api/graphql/dataloader/PostCommentsBatchLoaderTest.java
+++ app-service-comments/src/test/java/com/github/arhor/aws/graphql/federation/comments/api/graphql/dataloader/PostCommentsBatchLoaderTest.java
@@ -1,5 +1,29 @@
 package com.github.arhor.aws.graphql.federation.comments.api.graphql.dataloader;
 
+import com.github.arhor.aws.graphql.federation.comments.service.CommentService;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executor;
+
+import static org.mockito.Mockito.mock;
+
 class PostCommentsBatchLoaderTest {
 
+    private final Executor executor = mock();
+    private final CommentService commentService = mock();
+
+    private PostCommentsBatchLoader postCommentsBatchLoader = new PostCommentsBatchLoader(
+        executor,
+        commentService
+    );
+
+    @Test
+    void should_pass() {
+        // given
+
+        // when
+
+        // then
+
+    }
 }
