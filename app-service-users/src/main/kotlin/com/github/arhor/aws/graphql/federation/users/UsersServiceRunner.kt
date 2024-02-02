622965aebc33f52e5ef2866bc0bd23ba93b0ddc9
arhor <arhor18@gmail.com>
2024-02-02 16:57:40 +0100

    app config moved ouf runner itself;

--- app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/UsersServiceRunner.kt
+++ app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/UsersServiceRunner.kt
@@ -1,15 +1,8 @@
 package com.github.arhor.aws.graphql.federation.users
 
-import com.github.arhor.aws.graphql.federation.users.config.props.AppProps
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.retry.annotation.EnableRetry
-import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableRetry
-@EnableScheduling
-@EnableConfigurationProperties(AppProps::class)
 @SpringBootApplication
 class UsersServiceRunner
 
