a1c8a02ea1d9ce1d8bd48c5ebccdc4aa53351655
arhor <arhor18@gmail.com>
2024-05-11 16:44:55 +0200

    * corrected caching configuration;

--- app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/config/ConfigureCaching.kt
+++ app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/config/ConfigureCaching.kt
@@ -1,10 +1,15 @@
 package com.github.arhor.aws.graphql.federation.users.config
 
+import com.github.arhor.aws.graphql.federation.users.util.Caches
+import com.github.arhor.aws.graphql.federation.users.util.get
 import com.github.benmanes.caffeine.cache.Caffeine
 import graphql.ExecutionInput
 import graphql.execution.preparsed.PreparsedDocumentEntry
 import graphql.execution.preparsed.PreparsedDocumentProvider
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer
+import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
@@ -17,18 +22,24 @@ private typealias ParsingValidator = Function<ExecutionInput, PreparsedDocumentE
 class ConfigureCaching {
 
     @Bean
-    @Suppress("OVERRIDE_DEPRECATION")
-    fun asyncCachePreParsedDocumentProvider(): PreparsedDocumentProvider =
-        object : PreparsedDocumentProvider {
-            private val cache = buildCache<String, PreparsedDocumentEntry> {
-                maximumSize(250)
-                expireAfterAccess(Duration.ofMinutes(10))
+    fun caffeineCacheManagerCustomizer() = CacheManagerCustomizer<CaffeineCacheManager> {
+        it.registerCustomCache(
+            Caches.GRAPHQL_DOCUMENTS.name,
+            Caffeine.newBuilder()
+                .maximumSize(250)
+                .expireAfterAccess(Duration.ofMinutes(10))
+                .build()
+        )
+    }
+
+    @Bean
+    fun preparsedDocumentProvider(cacheManager: CacheManager): PreparsedDocumentProvider {
+        val cache = cacheManager[Caches.GRAPHQL_DOCUMENTS]
+
+        return PreparsedDocumentProvider { executionInput, parseAndValidateFunction ->
+            cache.get(executionInput.query) {
+                parseAndValidateFunction.apply(executionInput)
             }
-
-            override fun getDocument(execInput: ExecutionInput, validator: ParsingValidator) =
-                cache.get(execInput.query) { _ -> validator.apply(execInput) }
         }
-
-    private inline fun <K, V> buildCache(init: Caffeine<Any, Any>.() -> Unit) =
-        Caffeine.newBuilder().apply { init() }.build<K, V>()
+    }
 }
---
+++ app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/util/Caches.kt
@@ -0,0 +1,8 @@
+package com.github.arhor.aws.graphql.federation.users.util
+
+enum class Caches {
+    /**
+     * Cache for preparsed GraphQL documents.
+     */
+    GRAPHQL_DOCUMENTS,
+}
