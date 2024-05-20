eb9e23be83f3fd619f3eba0df4fc3a3bf03f0258
arhor <arhor18@gmail.com>
2024-05-20 18:12:58 +0200

    * added CacheManagerExtTest;

---
+++ app-service-users/src/test/kotlin/com/github/arhor/aws/graphql/federation/users/util/CacheManagerExtTest.kt
@@ -0,0 +1,65 @@
+package com.github.arhor.aws.graphql.federation.users.util
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchException
+import org.assertj.core.api.InstanceOfAssertFactories.throwable
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import org.springframework.cache.Cache
+import org.springframework.cache.CacheManager
+
+class CacheManagerExtTest {
+
+    private val cacheManager = mockk<CacheManager>()
+    private val cache = mockk<Cache>()
+
+    @Nested
+    @DisplayName("CacheManager :: get")
+    inner class GetTest {
+        @EnumSource
+        @ParameterizedTest
+        fun `should return expected cache instance when CacheManager#getCache returned not null`(
+            // Given
+            cacheName: Caches,
+        ) {
+            every { cacheManager.getCache(any()) } returns cache
+
+            // When
+            val result = cacheManager[cacheName]
+
+            // Then
+            verify(exactly = 1) { cacheManager.getCache(cacheName.name) }
+
+            assertThat(result)
+                .isNotNull()
+                .isEqualTo(cache)
+        }
+
+        @EnumSource
+        @ParameterizedTest
+        fun `should throw IllegalStateException when CacheManager#getCache returned null`(
+            // Given
+            cacheName: Caches,
+        ) {
+            val cacheManager = mockk<CacheManager>()
+
+            every { cacheManager.getCache(any()) } returns null
+
+            // When
+            val result = catchException { cacheManager[cacheName] }
+
+            // Then
+            verify(exactly = 1) { cacheManager.getCache(cacheName.name) }
+
+            assertThat(result)
+                .isNotNull()
+                .asInstanceOf(throwable(IllegalStateException::class.java))
+                .hasMessage("Cache $cacheName is not found!")
+        }
+    }
+}
