f290cd1e65e548e676b8e81b8d571eee21f31be7
arhor <arhor18@gmail.com>
2024-05-27 15:29:17 +0200

    * pagination WIP;

--- app-service-posts/src/main/kotlin/com/github/arhor/aws/graphql/federation/posts/util/PostsLookupInputExt.kt
+++
@@ -1,9 +0,0 @@
-package com.github.arhor.aws.graphql.federation.posts.util
-
-import com.github.arhor.aws.graphql.federation.posts.generated.graphql.types.PostsLookupInput
-
-val PostsLookupInput.limit
-    get() = size
-
-val PostsLookupInput.offset
-    get() = size * page
