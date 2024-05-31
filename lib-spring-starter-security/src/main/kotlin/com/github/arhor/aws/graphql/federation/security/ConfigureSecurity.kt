a5390e7acd9885d7ee51b1714c017c32d9385d7b
arhor <arhor18@gmail.com>
2024-05-31 15:11:10 +0200

    * fixed failed tests;

--- lib-spring-starter-security/src/main/kotlin/com/github/arhor/aws/graphql/federation/security/ConfigureSecurity.kt
+++ lib-spring-starter-security/src/main/kotlin/com/github/arhor/aws/graphql/federation/security/ConfigureSecurity.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguratio
 import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Lazy
 import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor
 import org.springframework.security.concurrent.DelegatingSecurityContextExecutor
 import java.util.concurrent.Executor
@@ -13,6 +14,7 @@ import java.util.concurrent.Executor
 @AutoConfiguration(before = [TaskExecutionAutoConfiguration::class])
 class ConfigureSecurity {
 
+    @Lazy
     @Bean(
         name = [
             TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME,
