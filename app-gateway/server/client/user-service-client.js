a4ba9bd4ae3e06d42278bb229eb6eb127498b871
arhor <arhor18@gmail.com>
2024-06-08 22:26:13 +0200

    * corrected auth function on the gateway;

--- app-gateway/server/client/user-service-client.js
+++ app-gateway/server/client/user-service-client.js
@@ -7,21 +7,41 @@ import { USERS_SERVICE_VERIFY_PATH } from '#server/utils/env.js';
  * @param {Object} credentials - The user's credentials.
  * @param {string} credentials.username - The username of the user to authenticate.
  * @param {string} credentials.password - The password of the user to authenticate.
- * @returns {Promise<any>} A promise that resolves to the authenticated user details if the authentication is successful.
+ * @returns {Promise<any>} An authenticated user details if the authentication is successful.
  */
 export async function authenticate({ username, password }) {
-    const response = await fetch(USERS_SERVICE_VERIFY_PATH, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password }),
-    });
-    if (!response.ok) {
-        const error = await response.json();
-        throw new GraphQLError(error.message, {
+    let success;
+    let failure;
+
+    try {
+        const response = await fetch(USERS_SERVICE_VERIFY_PATH, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password }),
+        });
+        const content = await response.json();
+
+        if (response.ok) {
+            success = content;
+        } else {
+            failure = {
+                message: content.message ?? 'Bad Credentials',
+                code: 'AUTHENTICATION_ERROR',
+            };
+        }
+    } catch (error) {
+        failure = {
+            message: error.message ?? 'Internal server error',
+            code: 'INTERNAL_SERVER_ERROR'
+        };
+    }
+
+    if (failure) {
+        throw new GraphQLError(failure.message, {
             extensions: {
-                code: 'BAD_USER_INPUT',
+                code: failure.code,
             },
         });
     }
-    return await response.json();
+    return success;
 }
