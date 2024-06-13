563f010c1f813881a0fb63ee65ed326c5b0c601e
arhor <arhor18@gmail.com>
2024-06-13 11:21:54 +0200

    * applied additional auth rule for the `user.authorities` field;

--- app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/infrastructure/graphql/datafetcher/AuthFetcher.kt
+++ app-service-users/src/main/kotlin/com/github/arhor/aws/graphql/federation/users/infrastructure/graphql/datafetcher/AuthFetcher.kt
@@ -1,12 +1,13 @@
 package com.github.arhor.aws.graphql.federation.users.infrastructure.graphql.datafetcher
 
 import com.github.arhor.aws.graphql.federation.starter.tracing.Trace
-import com.github.arhor.aws.graphql.federation.users.infrastructure.graphql.dataloader.AuthBatchLoader
 import com.github.arhor.aws.graphql.federation.users.generated.graphql.DgsConstants.USER
 import com.github.arhor.aws.graphql.federation.users.generated.graphql.types.User
+import com.github.arhor.aws.graphql.federation.users.infrastructure.graphql.dataloader.AuthBatchLoader
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsData
 import com.netflix.graphql.dgs.DgsDataFetchingEnvironment
+import org.springframework.security.access.prepost.PreAuthorize
 import java.util.UUID
 import java.util.concurrent.CompletableFuture
 
@@ -16,6 +17,7 @@ class AuthFetcher {
 
     /* ---------- Queries ---------- */
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DgsData(parentType = USER.TYPE_NAME, field = USER.Authorities)
     fun userAuthorities(dfe: DgsDataFetchingEnvironment): CompletableFuture<List<String>> {
         val loader = dfe.getDataLoader<UUID, List<String>>(AuthBatchLoader::class.java)
