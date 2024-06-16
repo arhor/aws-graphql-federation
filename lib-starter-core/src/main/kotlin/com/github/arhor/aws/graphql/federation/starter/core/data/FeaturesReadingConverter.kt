e2fee920a2787ca3046fddd8755b1ed991faad3a
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2024-06-16 23:53:33 +0200

    * corrected converter declaration for posts service;

--- lib-starter-core/src/main/kotlin/com/github/arhor/aws/graphql/federation/starter/core/data/FeaturesReadingConverter.kt
+++ lib-starter-core/src/main/kotlin/com/github/arhor/aws/graphql/federation/starter/core/data/FeaturesReadingConverter.kt
@@ -4,6 +4,25 @@ import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.ReadingConverter
 import java.util.EnumSet
 
+/**
+ * Must be extended to properly reify [F] generic type argument.
+ *
+ * Generics in Java are erased during compilation, the only exception I know - generic type arguments
+ * used during inheritance. So, it's impossible to reify generic type argument from the following
+ * declaration:
+ * ```java
+ * final var list = new ArrayList<Integer>();
+ * ```
+ *
+ * But, it's possible if we use inheritance:
+ * ```java
+ * class IntegerArrayList extends ArrayList<Integer> {}
+ * final var list = new IntegerArrayList();
+ * ```
+ *
+ * The same approach works with anonymous classes, since they syntax mixes object instantiation with
+ * class declaration.
+ */
 @ReadingConverter
 open class FeaturesReadingConverter<F : Features<F, E>, E : Enum<E>>(
     val type: Class<E>,
