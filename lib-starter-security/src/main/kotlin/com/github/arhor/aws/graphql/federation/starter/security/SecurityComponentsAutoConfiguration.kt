565291c49385c312bcaa8dd9c93dffafdf8bb082
arhor <arhor18@gmail.com>
2024-07-12 16:12:50 +0200

    * fixed an issue with uninitialized Executor;

--- lib-starter-security/src/main/kotlin/com/github/arhor/aws/graphql/federation/starter/security/SecurityComponentsAutoConfiguration.kt
+++ lib-starter-security/src/main/kotlin/com/github/arhor/aws/graphql/federation/starter/security/SecurityComponentsAutoConfiguration.kt
@@ -1,12 +1,20 @@
 package com.github.arhor.aws.graphql.federation.starter.security
 
+import org.springframework.beans.factory.BeanNameAware
+import org.springframework.beans.factory.DisposableBean
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME
 import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder
+import org.springframework.context.ApplicationContextAware
+import org.springframework.context.ApplicationListener
+import org.springframework.context.SmartLifecycle
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Lazy
-import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor
+import org.springframework.context.event.ContextClosedEvent
+import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME
 import org.springframework.security.concurrent.DelegatingSecurityContextExecutor
 import java.util.concurrent.Executor
 
@@ -14,14 +22,31 @@ import java.util.concurrent.Executor
 @AutoConfiguration(before = [TaskExecutionAutoConfiguration::class])
 class SecurityComponentsAutoConfiguration {
 
+    // What's happening here? Pretty easy to explain!
+    //
+    // I'm going to create my own bean representing Executor interface. Instead of using default
+    // one, I've chosen to use default ThreadPoolTaskExecutorBuilder, but wrapping built executor
+    // with DelegatingSecurityContextExecutor. Doing so I'm loosing all benefits of automatic bean
+    // lifecycle management provided by ThreadPoolTaskExecutor.
+    //
+    // So, I decided to redeclare explicitly interfaces implemented by ThreadPoolTaskExecutor,
+    // delegating their implementation to the wrapped instance, having at the same time all the
+    // logic from DelegatingSecurityContextExecutor. Voilà!
     @Lazy
     @Bean(
         name = [
-            TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME,
-            AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME,
+            APPLICATION_TASK_EXECUTOR_BEAN_NAME,
+            DEFAULT_TASK_EXECUTOR_BEAN_NAME,
         ]
     )
-    fun applicationTaskExecutor(builder: ThreadPoolTaskExecutorBuilder): Executor {
-        return DelegatingSecurityContextExecutor(builder.build())
-    }
+    fun applicationTaskExecutor(executor: ThreadPoolTaskExecutorBuilder): Executor =
+        executor.build().let {
+            object : DelegatingSecurityContextExecutor(it),
+                BeanNameAware by it,
+                ApplicationContextAware by it,
+                InitializingBean by it,
+                DisposableBean by it,
+                SmartLifecycle by it,
+                ApplicationListener<ContextClosedEvent> by it {}
+        }
 }
