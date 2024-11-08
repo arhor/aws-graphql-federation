249777dc0cc2b89e0ab5c631347710334ef8136d
arhor <arhor18@gmail.com>
2024-11-08 12:41:38 +0100

    refactoring

--- lib-starter-tracing/src/main/kotlin/com/github/arhor/aws/graphql/federation/starter/tracing/MethodExecutionLoggingAspect.kt
+++ lib-starter-tracing/src/main/kotlin/com/github/arhor/aws/graphql/federation/starter/tracing/MethodExecutionLoggingAspect.kt
@@ -6,19 +6,19 @@ import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.reflect.MethodSignature
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.event.Level
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 import java.util.concurrent.CompletionStage
-import kotlin.time.Duration
 
 @Aspect
 @Component
 @ConditionalOnProperty(prefix = "tracing.method-execution-logging", name = ["enabled"], havingValue = "true")
 class MethodExecutionLoggingAspect(
     tracingProperties: TracingProperties,
+    argFormattersList: List<ArgumentFormatter>,
 ) {
-    private val logLevel: Level = tracingProperties.methodExecutionLogging.level
+    private val loggingLvl = tracingProperties.methodExecutionLogging.level
+    private val formatters = argFormattersList.associateBy { it.argumentType }.withDefault { ArgumentFormatter.SIMPLE }
 
     // consider the following cases:
     //     1. return type is CompletionStage and joinPoint.proceed() was executed without exception and future contains success
@@ -32,48 +32,64 @@ class MethodExecutionLoggingAspect(
         val logger = LoggerFactory.getLogger(method.declaringTypeName)
 
         val methodName = method.name
-        val methodArgs = jPoint.args.contentToString()
+        val methodArgs = formatArgs(jPoint.args)
 
-        logger.atLevel(logLevel).log(EXECUTION_START, methodName, methodArgs)
+        if (logger.isEnabledForLevel(loggingLvl)) {
+            logger.write(EXECUTION_START, methodName, methodArgs)
 
-        return if (logger.isEnabledForLevel(logLevel)) {
             Timer.start {
                 try {
-                    when (val result = jPoint.proceed()) {
+                    return when (val result = jPoint.proceed()) {
                         is CompletionStage<*> -> {
                             result.whenComplete { success, failure ->
                                 if (failure != null) {
-                                    logger.failure(methodName, failure, elapsedTime)
+                                    logger.write(EXECUTION_ERROR, methodName, failure, elapsedTime)
                                 } else {
-                                    logger.success(methodName, success, elapsedTime)
+                                    logger.write(EXECUTION_CLOSE, methodName, success, elapsedTime)
                                 }
                             }
                         }
 
                         else -> {
-                            logger.success(methodName, result, elapsedTime)
+                            logger.write(EXECUTION_CLOSE, methodName, result, elapsedTime)
                             result
                         }
                     }
                 } catch (error: Throwable) {
-                    logger.failure(methodName, error, elapsedTime)
+                    logger.write(EXECUTION_ERROR, methodName, error, elapsedTime)
                     throw error
                 }
             }
         } else {
-            jPoint.proceed()
+            aspectLogger.warn("Logging level '{}' configured for method execution logs, but is not enabled", loggingLvl)
+            return jPoint.proceed()
         }
     }
 
-    private fun Logger.success(methodName: String, value: Any?, elapsedTime: Duration) {
-        atLevel(logLevel).log(EXECUTION_CLOSE, methodName, value, elapsedTime)
-    }
+    private fun formatArgs(args: Array<Any?>?): String =
+        if (args.isNullOrEmpty()) {
+            "[]"
+        } else {
+            args.joinToString(prefix = "[", postfix = "]", transform = ::formatArg)
+        }
 
-    private fun Logger.failure(methodName: String, error: Throwable, elapsedTime: Duration) {
-        atLevel(logLevel).log(EXECUTION_ERROR, methodName, error, elapsedTime)
-    }
+    private fun formatArg(arg: Any?): String =
+        if (arg == null) {
+            "null"
+        } else {
+            formatters.getValue(arg.javaClass).format(arg)
+        }
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun Logger.write(message: String, arg0: Any?, arg1: Any?): Unit =
+        atLevel(loggingLvl).log(message, arg0, arg1)
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun Logger.write(message: String, vararg args: Any?): Unit =
+        atLevel(loggingLvl).log(message, *args)
 
     companion object {
+        private val aspectLogger = LoggerFactory.getLogger(this::class.java.enclosingClass)
         private const val EXECUTION_START = "Method [START]: {}() >>> args: {}"
         private const val EXECUTION_CLOSE = "Method [CLOSE]: {}() <<< exit: {}, time: {}"
         private const val EXECUTION_ERROR = "Method [ERROR]: {}() <<< exit: {}, time: {}"
