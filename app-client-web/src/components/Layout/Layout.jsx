63fb852f79b91b75f8d36ea405407f23e1a71ac3
arhor <arhor18@gmail.com>
2024-11-25 16:36:59 +0100

    refactoring

--- app-client-web/src/components/Layout/Layout.jsx
+++ app-client-web/src/components/Layout/Layout.jsx
@@ -1,15 +1,18 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Container from '@mui/material/Container';
 
-import { Footer, Header } from '@/components';
+import { Footer, Header, Loader } from '@/components';
 
 export default function Layout() {
     return (
         <>
             <Header />
             <Container component="main" sx={{ p: 5 }}>
-                <Outlet />
+                <Suspense fallback={<Loader />}>
+                    <Outlet />
+                </Suspense>
             </Container>
             <Footer />
         </>
